c633c101f5abbd0b25f7fc84bb87b5e69f71dc5c
deadly6565 <129655946+deadly6565@users.noreply.github.com>
2023-04-02 15:25:01 +0500

    Add files via upload

---
+++ game.py
@@ -0,0 +1,27 @@
+
+from pygame import *
+class GameSprite(sprite.Sprite):
+    def __init__(self,sprite_image, x=0, y=0, width=50, height=50):
+        super().__init__()
+        self.image = transform.scale(image.load(sprite_image), (width, height))
+        self.rect = self.image.get_rect()
+        self.rect.x = x 
+        self.rect.y = y
+    def reset(self):
+        mw.blit(self.image, (self.rect.x, self.rect.y)) 
+
+FPS = 60
+BG_COLOR = (220, 100, 220)
+WIDTH, HEIGHT = 600, 480
+mw = display.set_mode((WIDTH, HEIGHT))
+display.set_caption('PING PONG')
+mw.fill(BG_COLOR)
+
+run = True
+While run:
+    
+    for e in event.get():
+        if e.type == quit:
+            run = False
+display.update()
+clock.tick(FPS)
